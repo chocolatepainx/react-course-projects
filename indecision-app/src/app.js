efc86c9a47adc2a499b52992c4b044193be612b9
TheDoc <johnle_10@hotmail.com>
2021-02-06 21:27:14 -0500

    app.js in src

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -3,11 +3,7 @@
 console.log('App.js is running!');
 
 // JSX - JavaScript XML
-var template = React.createElement(
-  'h1',
-  null,
-  'Indecision App'
-);
+var template = <h1>Indecision App</h1>;
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
